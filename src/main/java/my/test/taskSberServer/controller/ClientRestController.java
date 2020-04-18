076047e76c8f8de381a4e418b0119c956cd29d6f
nick <gnn@mm.st>
2020-04-18 16:44:27 +0300

    add controller

--- src/main/java/my/test/taskSberServer/controller/ClientRestController.java
+++ src/main/java/my/test/taskSberServer/controller/ClientRestController.java
@@ -1,4 +1,16 @@
 package my.test.taskSberServer.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/getMessages")
 public class ClientRestController {
+
+    @PostMapping(value = "/messageXML")
+    public void getMessage(@RequestBody String message) {
+        System.out.println(message);
+    }
 }
