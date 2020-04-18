be5ee2dbf4f964daf42ff9bf97c050af65d2f90c
nick <gnn@mm.st>
2020-04-18 21:53:47 +0300

    add dto

---
+++ src/main/java/my/test/taskSberServer/dto/MessageDto.java
@@ -0,0 +1,33 @@
+package my.test.taskSberServer.model;
+
+import java.util.List;
+
+public class MessageDto {
+    private long RqUID;
+    private String RqTm;
+    private List currency;
+
+    public long getRqUID() {
+        return RqUID;
+    }
+
+    public void setRqUID(long rqUID) {
+        RqUID = rqUID;
+    }
+
+    public String getRqTm() {
+        return RqTm;
+    }
+
+    public void setRqTm(String rqTm) {
+        RqTm = rqTm;
+    }
+
+    public List getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(List currency) {
+        this.currency = currency;
+    }
+}
