3ccaeb10ced64755f98b19766a03aa6f94e7d472
nick <gnn@mm.st>
2020-04-18 16:41:54 +0300

    add serv

---
+++ src/main/java/my/test/taskSberServer/TestTaskSberServerApp.java
@@ -0,0 +1,11 @@
+package my.test.taskSber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskSberServerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskSberServerApp.class, args);
+    }
+}
---
+++ src/main/java/my/test/taskSberServer/controller/ClientRestController.java
@@ -0,0 +1,4 @@
+package my.test.taskSberServer.controller;
+
+public class ClientRestController {
+}
